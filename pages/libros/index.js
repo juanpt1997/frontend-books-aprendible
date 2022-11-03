87f962cbb9e76c2062c3f578eb71e1acdc3697c5
juan.tabares <juan.tabares@coytex.com.co>
2022-11-03 14:35:30 -0500

    borrando libros de la base de datos

--- pages/libros/index.js
+++ pages/libros/index.js
@@ -14,6 +14,25 @@ export async function getStaticProps() {
 }
 
 const BookList = ({ books }) => {
+    async function handleDelete(e, bookId) {
+        e.preventDefault();
+
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/books/${bookId}`, {
+            method: 'POST',
+            headers: {
+                accept: 'application/json',
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify({
+                _method: 'DELETE'
+            })
+        })
+
+        if (res.ok) {
+            // Success
+            window.location.href = '/libros'
+        }
+    }
     return (
         <div>
             <h1>Libros</h1>
@@ -23,6 +42,10 @@ const BookList = ({ books }) => {
                         <Link href={`/libros/${book.id}`}>{book.title}</Link>
                         {' - '}
                         <Link href={`/libros/${book.id}/editar`}>Editar</Link>
+                        {' - '}
+                        <form onSubmit={(e) => handleDelete(e, book.id)} style={{ display: 'inline' }}>
+                            <button>Eliminar</button>
+                        </form>
                     </li>
                 ))}
             </ul>
